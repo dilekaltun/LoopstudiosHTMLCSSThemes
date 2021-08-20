fd5f2ad01c0e3240575821641495a24c610fa0ec
Dilek Altun <altyndilek@gmail.com>
2021-08-20 10:45:03 +0300

    Update

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+
 window.onscroll = function () { scrollFunction() };
         function scrollFunction() {
             document.getElementById("navbar").style.background = "#111";
+            var navbar = document.getElementById("navbar");
+            var navbarTop = navbar.offsetTop;
+            if (window.pageYOffset == navbarTop) {
+              document.getElementById("navbar").style.background = "none";
+            }
         }
       const navToggle = document.querySelector('.nav-toggle')
       const navLinks = document.querySelectorAll('.nav__link')
@@ -14,3 +20,4 @@ window.onscroll = function () { scrollFunction() };
           document.body.classList.remove('nav-open')
         })
       })
+
